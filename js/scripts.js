0d321b16bbd4bf7b5ee017362be365ecb10946f0
mfannick <mfannick1@gmail.com>
2019-07-20 21:43:53 +0200

    collecting data from HTML to Js using DOM

--- js/scripts.js
+++ js/scripts.js
@@ -3,5 +3,10 @@
     gender1=["female","male"];
     femaleNames=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"]
     maleNames=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"]
+
+    yy=document.getElementById("year").value;
+    cc=document.getElementById("century").value;
+    dd=document.getElementById("date").value;
+    mm=document.getElementById("month").value;
     };
     
