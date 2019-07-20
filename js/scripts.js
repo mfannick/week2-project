0098addf85d20f1373b27a5f2518f00661c33722
mfannick <mfannick1@gmail.com>
2019-07-20 21:42:18 +0200

    adding variables to be used.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
 
  function p(){
- 
+    gender1=["female","male"];
+    femaleNames=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"]
+    maleNames=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"]
     };
     
